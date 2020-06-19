4b4c128fe2df22f78a759e8b9ee9ac541c119810
Dan Lynch <pyramation@gmail.com>
2020-06-19 00:35:41 -0700

    administrator role

--- bootstrap-roles.sql
+++ bootstrap-roles.sql
@@ -2,5 +2,11 @@ CREATE ROLE administrator;
 CREATE ROLE anonymous;
 CREATE ROLE authenticated;
 CREATE ROLE app_user LOGIN PASSWORD 'app_password';
+
+GRANT anonymous TO administrator;
+GRANT authenticated TO administrator;
+GRANT administrator TO app_user;
+ALTER ROLE administrator BYPASSRLS;
+
 GRANT anonymous TO app_user;
 GRANT authenticated TO app_user;
