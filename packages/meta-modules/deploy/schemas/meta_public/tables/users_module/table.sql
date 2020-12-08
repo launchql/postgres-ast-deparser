37fda61bdf9eab01750f8bb13d0e56a88afefbf7
Dan Lynch <pyramation@gmail.com>
2020-12-07 21:37:51 -0700

    goods

--- packages/meta-modules/deploy/schemas/meta_public/tables/users_module/table.sql
+++ packages/meta-modules/deploy/schemas/meta_public/tables/users_module/table.sql
@@ -4,6 +4,11 @@
 
 BEGIN;
 
+-- ARGUMENT for using this instead...
+-- because! you're already adding a function modules_private.users_module
+-- in other words, you are already adding SQL code... so just do 
+-- real SQL instead!
+
 CREATE TABLE meta_public.users_module (
     id uuid PRIMARY KEY DEFAULT uuid_generate_v4 (),
     api_id uuid NOT NULL REFERENCES meta_public.apis (id),
