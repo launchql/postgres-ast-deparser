7a110626e682290ae49b43268e7530fd26c76562
Dan Lynch <pyramation@gmail.com>
2020-12-30 06:24:18 -0700

    sql

---
+++ bootstrap-test-roles.sql
@@ -0,0 +1,11 @@
+CREATE ROLE app_user LOGIN PASSWORD 'app_password';
+GRANT anonymous TO app_user;
+GRANT authenticated TO app_user;
+
+-- admin user
+CREATE ROLE app_admin LOGIN PASSWORD 'admin_password';
+GRANT anonymous TO administrator;
+GRANT authenticated TO administrator;
+GRANT administrator TO app_admin;
+
+ALTER USER app_admin WITH CREATEDB;
