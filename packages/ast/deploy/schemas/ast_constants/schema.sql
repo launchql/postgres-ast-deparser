559e902f7bbd1ec6ecb9e2f0dcb19360533a3cc6
Dan Lynch <pyramation@gmail.com>
2020-12-30 08:59:10 -0700

    grants

--- packages/ast/deploy/schemas/ast_constants/schema.sql
+++ packages/ast/deploy/schemas/ast_constants/schema.sql
@@ -4,5 +4,10 @@
 BEGIN;
 
 CREATE SCHEMA ast_constants;
+GRANT USAGE ON SCHEMA ast_constants TO public;
+
+ALTER DEFAULT PRIVILEGES IN SCHEMA ast_constants
+GRANT EXECUTE ON FUNCTIONS
+TO public;
 
 COMMIT;
